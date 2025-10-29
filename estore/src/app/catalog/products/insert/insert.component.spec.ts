115693d1fb50e398ad1394510da0fba905da447d
amarshraddhachinchane <amarshraddhachinchane@gmail.com>
2025-10-29 10:20:09 +0530

    merged Ravi src

--- estore/src/app/catalog/products/insert/insert.component.spec.ts
+++ estore/src/app/catalog/products/insert/insert.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { CreateComponent } from './create.component';
+import { InsertComponent } from './insert.component';
 
-describe('CreateComponent', () => {
-  let component: CreateComponent;
-  let fixture: ComponentFixture<CreateComponent>;
+describe('InsertComponent', () => {
+  let component: InsertComponent;
+  let fixture: ComponentFixture<InsertComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [CreateComponent]
+      imports: [InsertComponent]
     })
     .compileComponents();
     
-    fixture = TestBed.createComponent(CreateComponent);
+    fixture = TestBed.createComponent(InsertComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
